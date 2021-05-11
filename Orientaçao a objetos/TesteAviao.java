81d5a4eb5f0dcc5b647ad61c4b125e7587144457
Isaifo <ysoca98@hotmail.com>
2021-05-10 22:56:25 -0300

    Exercicios de Orientação a objetos

---
+++ Orientaçao a objetos/TesteAviao.java
@@ -0,0 +1,11 @@
+
+public class TesteAviao {
+			
+	public static void main(String[] args) {
+		
+		Aviao aviao1 = new Aviao("Boeing","Gol");
+		
+		System.out.println(aviao1.getInfoCompleta());
+	
+	}
+}
---
+++ Orientaçao a objetos/TesteCliente.java
@@ -0,0 +1,12 @@
+
+public class TesteCliente {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cliente cliente1 = new Cliente("Angelo",1234);
+		cliente1.compra();
+		System.out.println(cliente1.getInfo());
+	}
+
+}
+
